84d0084be3a6b08a4f25c26b3b11fad0a9808d8e
Oliver <oliver22official@gmail.com>
2023-03-27 18:39:51 +0200

    a

--- vista1.js
+++ vista1.js
@@ -12,7 +12,7 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 function mostrarPokemon(pokemon){ /*datos de un pokemon*/
 const div = document.createElement("div");
 div.classList.add("pokemon");
-div.innerHTML = `<img src="${pokemon.sprites.front_default}" class="imagen-pokemons" alt="ImagenPokemons">
+div.innerHTML = `<img src="${pokemon.sprites.other["official-artwork"].front_default}" class="imagen-pokemons" alt="ImagenPokemons">
                 <p><span class="numero-pokemons">N&deg; ${(pokemon.id.toString().padStart(3,0))}</span>  ${pokemon.name}</p>
                 <p>Tipo del pok&eacute;mon</p>`;
 listaPokemon[0].append(div);
