06527ac1393a9c526e01f793b4c476cbcc219c05
Oliver <oliver22official@gmail.com>
2023-03-29 19:47:33 +0200

    id

--- vista1.js
+++ vista1.js
@@ -1,4 +1,4 @@
-const listaPokemon = document.getElementsByClassName("lista-pokemon");
+const listaPokemon = document.getElementById("lista-pokemon");
 
 for(let i = 1; i <= 151; i++){
 datosPokemons(i);
@@ -20,7 +20,7 @@ div.classList.add("pokemon");
 div.innerHTML = `<img src="${pokemon.sprites.other["official-artwork"].front_default}" class="imagen-pokemons" alt="ImagenPokemons">
                 <p><span class="numero-pokemons">N&deg; ${(pokemon.id.toString().padStart(3,0))}</span>  ${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</p>
                 ${tipos}`;
-listaPokemon[0].append(div);
+listaPokemon.append(div);
 }
 
 
