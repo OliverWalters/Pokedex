61af99adb77d32b38654f317bb69113d17234c63
Albertsak <albertog300604@gmail.com>
2023-03-29 19:40:07 +0200

    Update vista1.js

--- vista1.js
+++ vista1.js
@@ -1,4 +1,5 @@
-const listaPokemon = document.getElementsByClassName("lista-pokemon")
+const listaPokemon = document.getElementsByClassName("lista-pokemon");
+
 for(let i = 1; i <= 151; i++){
 datosPokemons(i);
 }
@@ -11,13 +12,13 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 
 function mostrarPokemon(pokemon){ /*datos de un pokemon*/
     
-let tipos = pokemon.types.map(type => `<span class="${type.type.name} tipos"> ${type.type.name}</span>`);
+let tipos = pokemon.types.map(type => `<span class="${type.type.name} tipos"> ${type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)}</span>`);
 tipos = tipos.join(''); 
 
 const div = document.createElement("div");
 div.classList.add("pokemon");
 div.innerHTML = `<img src="${pokemon.sprites.other["official-artwork"].front_default}" class="imagen-pokemons" alt="ImagenPokemons">
-                <p><span class="numero-pokemons">N&deg; ${(pokemon.id.toString().padStart(3,0))}</span>  ${pokemon.name}</p>
+                <p><span class="numero-pokemons">N&deg; ${(pokemon.id.toString().padStart(3,0))}</span>  ${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</p>
                 ${tipos}`;
 listaPokemon[0].append(div);
 }
