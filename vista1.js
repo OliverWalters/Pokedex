4553972e1905b796bfa5da5adecb638126ccfd7a
Veronica alejandra toro gomez <114920562+Veronicatorogomez@users.noreply.github.com>
2023-03-30 12:29:51 +0200

    CAMBIOS 4

--- vista1.js
+++ vista1.js
@@ -12,37 +12,41 @@ fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
 
 function mostrarPokemon(pokemon){ /*datos de un pokemon*/
 const tiposTraducidos = {
-    normal: 'normal',
-    fire: 'fuego',
-    water: 'agua',
-    electric: 'eléctrico',
-    grass: 'planta',
-    ice: 'hielo',
-    fighting: 'lucha',
-    poison: 'veneno',
-    ground: 'tierra',
-    flying: 'volador',
-    psychic: 'psíquico',
-    bug: 'bicho',
-    rock: 'roca',
-    ghost: 'fantasma',
-    dragon: 'dragón',
-    dark: 'siniestro',
-    steel: 'acero',
-    fairy: 'hada'
+    normal: 'Normal',
+    fire: 'Fuego',
+    water: 'Agua',
+    electric: 'Eléctrico',
+    grass: 'Planta',
+    ice: 'Hielo',
+    fighting: 'Lucha',
+    poison: 'Veneno',
+    ground: 'Tierra',
+    flying: 'Volador',
+    psychic: 'Psíquico',
+    bug: 'Bicho',
+    rock: 'Roca',
+    ghost: 'Fantasma',
+    dragon: 'Dragón',
+    dark: 'Siniestro',
+    steel: 'Acero',
+    fairy: 'Hada'
   };
 
-
-
-let tipos = pokemon.types.map(type => `<span class="${type.type.name} tipos"> ${tiposTraducidos[type.type.name].charAt(0).toUpperCase() + tiposTraducidos[type.type.name].slice(1)}</span>`);
+let tipos = pokemon.types.map(type => `<span class="${type.type.name} tipos"> ${tiposTraducidos[type.type.name]}</span>`);
 tipos = tipos.join(''); 
 
 const div = document.createElement("div");
-div.classList.add("pokemon");
-div.innerHTML = `<img src="${pokemon.sprites.other["official-artwork"].front_default}" class="imagen-pokemons" alt="ImagenPokemons">
+div.innerHTML = `<button type="button" class="boton" onclick= redireccion(${pokemon.id})>
+                <div class="pokemon">
+                <img src="${pokemon.sprites.other["official-artwork"].front_default}" class="imagen-pokemons" alt="ImagenPokemons">
                 <p><span class="numero-pokemons">N&deg; ${(pokemon.id.toString().padStart(3,0))}</span>  ${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</p>
-                ${tipos}`;
+                ${tipos}
+                </div> 
+                </button>`;
 listaPokemon.append(div);
+}
+function redireccion(id){
+  location.href = `PokedexVista2.html?id=${id}`;
 }
 
 
